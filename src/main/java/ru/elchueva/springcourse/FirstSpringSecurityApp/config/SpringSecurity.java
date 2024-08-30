d15f9b0aa174042e3387d73e7a31bd50a59e27e8
”Sabina <sabina.elchueva@gmail.com>
2024-08-30 14:58:08 +0300

    lesson 87 authorization with @PreAuthorize

--- src/main/java/ru/elchueva/springcourse/FirstSpringSecurityApp/config/SpringSecurity.java
+++ src/main/java/ru/elchueva/springcourse/FirstSpringSecurityApp/config/SpringSecurity.java
@@ -216,6 +216,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -225,6 +226,7 @@ import ru.elchueva.springcourse.FirstSpringSecurityApp.services.PersonDetailsSer
 
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SpringSecurity {
 
 
@@ -245,7 +247,7 @@ public class SpringSecurity {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests((requests) -> requests
-                        .requestMatchers("/admin").hasRole("ADMIN")
+                        //.requestMatchers("/admin").hasRole("ADMIN")
                         .requestMatchers("/auth/login", "/error", "/auth/registration").permitAll()
                         .anyRequest().authenticated()
                 )
--- src/main/java/ru/elchueva/springcourse/FirstSpringSecurityApp/controllers/HelloController.java
+++ src/main/java/ru/elchueva/springcourse/FirstSpringSecurityApp/controllers/HelloController.java
@@ -1,5 +1,6 @@
 package ru.elchueva.springcourse.FirstSpringSecurityApp.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationTrustResolver;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,10 +8,19 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import ru.elchueva.springcourse.FirstSpringSecurityApp.security.PersonDetails;
+import ru.elchueva.springcourse.FirstSpringSecurityApp.services.AdminService;
 import ru.elchueva.springcourse.FirstSpringSecurityApp.services.PersonDetailsService;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
+
     @GetMapping("/hello")
     public String sayHello(){
         return "hello";
@@ -27,6 +37,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage(){
+        adminService.doAdminStaff();
         return "admin";
     }
 }
